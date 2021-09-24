fd4b42e79aff5230c21f891d84876e2d075485a3
MincheolKwon <ventania1680@seerslab.com>
2021-09-24 10:55:31 +0900

    fix: summoner name uri encode

--- src/api/v1/summoners.ts
+++ src/api/v1/summoners.ts
@@ -6,7 +6,7 @@ import { ISummoner } from '../../models/summoner.model'
 const router = Router();
 
 router.get('/:summonerName', async (req: Request, res: Response) => {
-  const summonerName = req.params.summonerName;
+  const summonerName = encodeURIComponent(req.params.summonerName);
   const summoner: ISummoner = await SummonerService.findByName(summonerName) as ISummoner;
   const match = await MatchService.findByPuuid(summoner.puuid);
   res.status(200).json({ errorCode: 200, summoner: summoner, match: match });
